8c09ee15b2f006e132f335ed608f01c4778e4def
niazmurshedsiam <niazmurshed16@gmail.com>
2021-04-27 13:23:49 +0600

    Use Styled Component to apply style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Header2 from './components/Home/Header2/Header2';
 import Home from './components/Home/Home';
 
 function App() {
+  
   return (
     <div>
       <Header></Header>
+      <Header2></Header2>
       <Home></Home>
       <Footer></Footer>
     </div>
--- src/components/Home/Header2/Header2.js
+++ src/components/Home/Header2/Header2.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header2 = () => {
+    const Title =styled.h1 `
+        font-size: 50px;
+        border : 1px solid green;
+        margin:5px;
+        padding:5px
+        text-align: center;
+        color: palevioletred;
+    `;
+    const Container = styled.div `
+        border : 1px solid green;
+        margin:5px;
+        padding:5px;
+        text-align:center;
+        color: blue;
+    `;
     return (
         <div>
-            <h3>This is Header 2</h3>
+            <Title>This is Header 3</Title>
+            <Container>
+                <h3>Product Name </h3>
+                <p>Product Description</p>
+            </Container>
+            <Container>
+                <h3>Product Name </h3>
+                <p>Product Description</p>
+            </Container>
+            <Container>
+                <h3>Product Name </h3>
+                <p>Product Description</p>
+            </Container>
+            <Container>
+                <h3>Product Name </h3>
+                <p>Product Description</p>
+            </Container>
         </div>
+        
+        
     );
 };
 
-export default Header2;<h3>This is Header 2</h3>
+export default Header2;
